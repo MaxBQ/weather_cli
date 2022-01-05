83f9d78682fae35833f22fefc0c55ec42c7dfd9e
MaxBQ <maksat.bek93@gmail.com>
2022-01-05 14:05:36 +0500

    add: api

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
+import { getWeather } from './services/api.service.js'
 import { printError, printHelp, printSuccess } from './services/log.service.js'
-import { saveKeyValue } from './services/storage.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан токен')
+  }
   try {
-    await saveKeyValue('token', token)
+    await saveKeyValue(TOKEN_DICTIONARY.token, token)
     printSuccess('Токен сохранен')
   } catch (err) {
     printError(err.message)
@@ -14,7 +18,6 @@ const saveToken = async (token) => {
 
 const initCLI = () => {
   const args = getArgs(process.argv)
-  console.log(args)
   if (!!args.h) {
     printHelp()
   }
@@ -24,7 +27,7 @@ const initCLI = () => {
   if (!!args.t) {
     saveToken(args.t)
   }
-  //weather
+  getWeather('Ashgabat')
 }
 
 initCLI()
